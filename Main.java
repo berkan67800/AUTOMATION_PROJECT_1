958c7a5d4d96b456644a7bfaf41212ef964f9ae8
Berkan <bsaridilek@gmail.com>
2022-09-28 00:35:28 -0700

    First Automation assignment

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","/Users/berkansaridilek/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/Users/bbbbb/Downloads/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://qa-duotify.us-east-2.elasticbeanstalk.com/register.php");
         String title = driver.getTitle();
